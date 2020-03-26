f65bdc3b2bcb3f48804f81fc65371aab6b8b3a11
david <47777979+David2he@users.noreply.github.com>
2020-03-26 22:48:01 +0100

    Revert "Merge branch 'master' of https://github.com/charlotteblassel/three.js_museum"
    This reverts commit 1378f1d71c3baa80f4307633a8a5361b6a8a7e01, reversing
    changes made to 658489c7a23f6f2c54f8f15edb64ba5fa8a49556.

--- src/index.js
+++ src/index.js
@@ -16,42 +16,6 @@ import Xylophone from "./scripts/Xylophone.js";
 import Bassviolin from "./scripts/Bassviolin.js";
 import Sky from "./assets/sky.jpg";
 
-
-// BLOCKER PAGE 
-
-const realisation = document.querySelector('.realisation')
-const forwardControlInstruction = document.querySelector('.forwardControlInstruction')
-const leftControlInstruction = document.querySelector('.leftControlInstruction')
-const backwardControlInstruction = document.querySelector('.backwardControlInstruction')
-const rightControlInstruction = document.querySelector('.rightControlInstruction')
-const spaceControlInstruction = document.querySelector('.spaceControlInstruction')
-const escControlInstruction = document.querySelector('.escControlInstruction')
-const SQSKey = document.querySelector('.SQSKey')
-const espaceKey = document.querySelector('.espaceKey')
-const escKey = document.querySelector('.escKey')
-
-
-let transition = setTimeout(
-    function (){
-        realisation.style.opacity='0'
-    },4000
-)
-let transition2 = setTimeout(
-    function (){
-        realisation.textContent='Appuyez sur ESPACE pour entrer dans le musée'
-        realisation.style.opacity='1'
-        realisation.style.border='1px solid red'
-    },5000
-)
-
-forwardControlInstruction.addEventListener(
-    'click',
-    ()=>{
-        window.alert('oij')
-    }
-)
-
-
 const blocker = document.querySelector(".blocker");
 
 /**
@@ -848,5 +812,3 @@ const loop = () => {
 loop();
 
 window.addEventListener("click", () => {});
-
-
